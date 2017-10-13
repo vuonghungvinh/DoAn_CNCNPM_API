4bc9aa25f09146bfb4767abbad4f9479c92a5c62
Vuong <vuonghungvinhit@gmail.com>
2017-10-13 19:28:09 +0700

    add bootstrap into app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="16x16">
         <title>Laravel5A2</title>
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js" type="text/javascript"></script>
     </head>
     <body>
         <app-root></app-root>
