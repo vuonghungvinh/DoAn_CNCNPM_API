db7cca2148d7d6c7357bf4129dcc48cc1efa3392
vietvu <levietvu1995@gmail.com>
2017-10-25 11:21:56 +0700

    two page themcauhoi, xemnganhangcauhoi

--- resources/assets/src/app/ThemSinhVien/themsinhvien.component.ts
+++ resources/assets/src/app/ThemSinhVien/themsinhvien.component.ts
@@ -7,13 +7,13 @@ import { SinhvienService } from '../services/sinhvien.service';
   styleUrls: ['./themsinhvien.component.scss'],
   providers: [ SinhvienService ]
 })
-export class ThemSinhVien  { 
+export class ThemSinhVien  {
 	constructor(
 		private sinhvienservice :SinhvienService,
 		private router :Router){}
 	public students:any[];
-	
-	
+
+
 	ngOnInit(){
 		//lay danh sach lop da co bo vao select box
 		this.sinhvienservice.getdanhsachsinhvien().subscribe(data =>{
@@ -24,9 +24,12 @@ export class ThemSinhVien  {
 	addstudents(value){
 	console.log(value);
 	this.sinhvienservice.addstudents(value).subscribe(data=>{
-		console.log(data);
+    console.log(data);
+    alert("Thêm sinh viên thành công");
+    this.router.navigate(['/question']);
+
 	});
 	// this.router.navigate(['xemdanhsach']);
-		
+
 	}
 }
