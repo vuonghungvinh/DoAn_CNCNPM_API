b4126e3906f6365348fcbd8dd5b3bb3d1c8a76fe
vietvu <levietvu1995@gmail.com>
2017-11-27 17:45:51 +0700

    sua giao dien them sinh vien vao lop hoc phan

--- resources/assets/src/app/ThemSinhVienVaoLopHocPhan/themsinhvienvaolophocphan.component.ts
+++ resources/assets/src/app/ThemSinhVienVaoLopHocPhan/themsinhvienvaolophocphan.component.ts
@@ -19,6 +19,9 @@ export class ThemsinhvienvaolophocphanComponent {
     public idmon: number;
     public submit = true;
     public malophp: string;
+    public checkclick = true;
+    public namebutton = 'Chọn tất cả';
+    public listsinhvien = [];
     public id_mon: any;
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
@@ -28,6 +31,32 @@ export class ThemsinhvienvaolophocphanComponent {
     this.lophocphan.getlophocphanchuadkthi(this.id_mon).subscribe( data => {
       this.mon = data['lophocphan'];
     });
+  }
+  alllistsinhvien(value: any) {
+    if (this.listsinhvien.indexOf(value) >= 0 ) {
+      this.listsinhvien.splice(this.listsinhvien.indexOf(value), 1);
+    }
+    // tslint:disable-next-line:one-line
+    else {
+      this.listsinhvien.push(value);
+    }
+  }
+  chontatca() {
+    if (this.checkclick) {
+      this.namebutton = 'Bỏ chọn';
+      this.checkclick = false;
+      this.sinhvien.forEach(element => {
+        if (this.listsinhvien.indexOf(element.mssv) === -1) {
+          this.listsinhvien.push(element.mssv);
+        }
+      });
+    }
+    // tslint:disable-next-line:one-line
+    else {
+      this.namebutton = 'Chọn tất cả';
+      this.checkclick = true;
+      this.listsinhvien.splice(0, this.listsinhvien.length);
+    }
   }
   getMaLopHP(event) {
     this.mon.forEach(element => {
@@ -51,17 +80,22 @@ export class ThemsinhvienvaolophocphanComponent {
     // });
   }
   themSinhVien(value: any) {
+    console.log(this.listsinhvien);
     value['mamon'] = this.idmon;
     value['malophp'] = this.malophp;
-    if (this.submit) {
-      this.submit = false;
-      this.lophocphan.addSinhVienVaoLopHP(value).subscribe( data => {
-        alert('Thêm  thành công.');
-        this.router.navigate(['/xemdanhsachmon']);
-      });
+    value['listsinhvien'] = this.listsinhvien;
+    if (this.listsinhvien.length === 0) {
+      alert('Vui lòng chọn ít nhất 1 sinh viên');
     }
-
-    console.log(value);
-
+    // tslint:disable-next-line:one-line
+    else {
+      if (this.submit) {
+        this.submit = false;
+        this.lophocphan.addSinhVienVaoLopHP(value).subscribe( data => {
+          alert('Thêm  thành công.');
+          this.router.navigate(['/xemdanhsachmon']);
+        });
+      }
+    }
   }
 }
