0ce6026861f8a6418a3254e20efd4efad37ef8ec
Vuong <vuonghungvinhit@gmail.com>
2017-11-30 04:16:09 +0700

    update xem lai chi tiet bai thi

--- resources/assets/src/app/xemchitietbaithi/xemchitietbaithi.component.ts
+++ resources/assets/src/app/xemchitietbaithi/xemchitietbaithi.component.ts
@@ -7,6 +7,7 @@ import { XemLaiBaiThi } from '../services/xemlaibaithi.service';
 @Component({
     selector: 'xem-lai-bai-thi',
     templateUrl: './xemchitietbaithi.component.html',
+    styleUrls: ['./xemchitietbaithi.component.scss'],
     encapsulation: ViewEncapsulation.None,
     providers: [XemLaiBaiThi]
 })
@@ -14,7 +15,7 @@ import { XemLaiBaiThi } from '../services/xemlaibaithi.service';
 // tslint:disable-next-line:component-class-suffix
 export class XemChiTietBaiThi implements OnInit, OnDestroy {
     private subscription: Subscription[] = [];
-
+    public data;
     public constructor(
         private activatedRoute: ActivatedRoute,
         private xemlaibaithiService: XemLaiBaiThi
@@ -25,9 +26,44 @@ export class XemChiTietBaiThi implements OnInit, OnDestroy {
             this.activatedRoute.params.subscribe(params => {
                 this.xemlaibaithiService.detail(params.id).subscribe(data => {
                     console.log(data);
+                    this.data = data;
                 });
             })
         );
+    }
+
+    xuLyDapAn(idcauhoi, dapandung, dapantraloi, id_dapan) {
+        id_dapan = id_dapan.toString();
+        if (dapandung[dapandung.length - 1] === ',') {
+            dapandung = dapandung.substring(0, dapandung.length - 1);
+        }
+        const listdapandung = dapandung.split(',');
+        const listdapantraloi = dapantraloi.split(';');
+        let listanswers = [];
+        for (let i = 0; i < listdapantraloi.length; i += 1) {
+            const tmp = listdapantraloi[i].split(':');
+            if (tmp[0] == idcauhoi) {
+                listanswers = tmp[1].split(' ');
+            }
+        }
+        let result = '';
+        if (listanswers.length < 1 && listdapandung.indexOf(id_dapan) >= 0) {
+            console.log('correct');
+            return 'correct';
+        }
+        if (listanswers.indexOf(id_dapan) >= 0) {
+            if (listdapandung.indexOf(id_dapan) >= 0) {
+                console.log('right');
+                return 'right';
+            }
+            return 'wrong';
+        }
+        console.log('herererer');
+        console.log(listdapandung);
+        if (listdapandung.indexOf(id_dapan) >= 0) {
+            return 'correct';
+        }
+        return '';
     }
 
     ngOnDestroy() {
